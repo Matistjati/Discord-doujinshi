5244c80a9c098c3758fc65108e52267953890473
Jaushau <matistjati@outlook.com>
2020-07-31 14:45:52 +0200

    More heroku debugging

--- UwUreader.py
+++ UwUreader.py
@@ -121,6 +121,8 @@ async def on_message(message):
 
     print(message)
     if message.content.startswith(prefix + 'view'):
+        msg1 = await message.channel.send(requests.get("hi")
+        instance.assign_msg(msg1)
         msg = await message.channel.send(requests.get("https://jsonplaceholder.typicode.com/posts/1").json()["title"])
         instance.assign_msg(msg)
         content = message.content.split()
